6a5901f0f2b16449bcd556d127a15c5a4bdc4ef5
Libernia Waruwu <waruwulibernia@gmal.com>
2023-10-06 21:40:11 +0700

    Update

--- js/javascript.js
+++ js/javascript.js
@@ -53,57 +53,61 @@ $(document).ready(function () {
 
 const BASE_URL = 'http://localhost:3000'
 
-function inputDataBarang(event){
+function inputDataBarang(event) {
     const namaPengguna = document.querySelector("#nama-pengguna")
     const nomorHP = document.querySelector("#nomorhp")
     const namaBarang = document.querySelector("#nama-barang")
-    const alamatTujuan = document.querySelector("#alamatujuan")
+    const alamatTujuan = document.querySelector("#alamat-tujuan")
     const email = document.querySelector("#email")
 
     fetch(`${BASE_URL}/input-data-barang`, {
-        method:'POST',
-        headers:{
-            "Content-Type":"application/json"
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
         },
-        body:JSON.stringify({
-            nama:namaPengguna.value,
-            nomorHP:nomorHP.value,
-            email:email.value,
-            alamatTujuan:alamatTujuan.value,
-            namaBarang:namaBarang.value,
+        body: JSON.stringify({
+            nama: namaPengguna.value,
+            nomorHP: nomorHP.value,
+            email: email.value,
+            alamatTujuan: alamatTujuan.value,
+            namaBarang: namaBarang.value,
         }),
     })
-      .then(response => response.json())
-      .then((response) => {
-        alert(`Data berhasil diinputkan, nomor resi anda adalah$(response.nomorResi)`
-        );
-        namaPengguna.value = '';
-        nomorHP.value = '';
-        namaBarang.value = '';
-        alamatTujuan.value = '';
-        email.value = '';
-    })
-    .catch(error => {
-        console.error(error);
-    });
+        .then(response => response.json())
+        .then((response) => {
+            alert(`Data berhasil diinputkan, nomor resi anda adalah ${response.nomorResi}`);
+            namaPengguna.value = '';
+            nomorHP.value = '';
+            namaBarang.value = '';
+            alamatTujuan.value = '';
+            email.value = '';
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
-function cekResi(event){
+function cekResi(event) {
     const nomorResi = document.querySelector("#input-cek-resi")
     fetch(`${BASE_URL}/cek-resi/${nomorResi.value}`)
-       
-      .then(response => response.json())
-      .then((response) => {
-        document.querySelector("#content-cek-resi").innerHTML = `
-        <ul>
-             <li style="list-style: none">Nama:${response.data.nama}</li>
-             <li style="list-style: none">Email:${response.data.email}</li>
-             <li style="list-style: none">Nama Barang:${response.data.namaBarang}</li>
-             <li style="list-style: none">Nomor HP:${response.data.nomorHP}</li>
-        </ul>
-        `;
-    })
-    .catch(error => {
-        console.error(error);
-    });
+
+        .then(response => response.json())
+        .then((response) => {
+            if(response.data){
+                document.querySelector("#content-cek-resi").innerHTML = `
+                <ul>
+                     <li style="list-style: none">Nama:${response.data.nama}</li>
+                     <li style="list-style: none">Email:${response.data.email}</li>
+                     <li style="list-style: none">Nama Barang:${response.data.namaBarang}</li>
+                     <li style="list-style: none">Nomor HP:${response.data.nomorHP}</li>
+                </ul>
+                `;
+            }else{ 
+                alert("Nomor resi tidak ditemukan")
+            }
+           
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
